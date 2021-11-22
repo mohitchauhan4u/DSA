682396200f3322c2b9a7bc9b04388f7a11fb4737
mohitchauhan4u <mohitchauhan1929@gmail.com>
2021-11-22 14:50:33 +0530

    pnc question in cp lab

---
+++ src/CP_Lab/PermutationAndCombination.java
@@ -0,0 +1,16 @@
+package CP_Lab;
+
+public class PermutationAndCombination {
+    public static void main(String[] args) {
+        stringCombinations("abc","");
+    }
+    public static void stringCombinations(String ques,String ans){
+        if(ques.length()==0){
+            System.out.print(ans+" ");
+            return;
+        }
+        for(int i=0;i<ques.length();i++){
+            stringCombinations(ques.substring(i+1),ans+ques.charAt(i));
+        }
+    }
+}
