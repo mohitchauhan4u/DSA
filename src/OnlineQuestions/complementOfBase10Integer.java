f7e2c2b6452d64eff7f1085a62beb1447555ec7c
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-04 13:22:26 +0530

    solved leetcode question bitwiseComplement

--- src/OnlineQuestions/complementOfBase10Integer.java
+++ src/OnlineQuestions/complementOfBase10Integer.java
@@ -2,21 +2,21 @@ package OnlineQuestions;
 
 public class complementOfBase10Integer {
 
-    //wrong solution
     public static int bitwiseComplement(int n) {
-        String str=Integer.toBinaryString(n);
-        String comp="";
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)=='1'){
-                str+='0';
-            }else{
-                str+='1';
+        String str = Integer.toBinaryString(n);
+        StringBuilder comp = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1') {
+                comp.append('0');
+            } else {
+                comp.append('1');
             }
         }
-        return Integer.parseInt(str, 2);
+        return Integer.parseInt(comp.toString(), 2);
     }
 
     public static void main(String[] args) {
+        System.out.println(bitwiseComplement(50));
         System.out.println(bitwiseComplement(5));
     }
 }
