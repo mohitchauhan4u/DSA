68a616dc7e2e7705afd4041835bc23d18ccd661e
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-14 10:31:52 +0530

    changed data type to long

--- src/DynamicProgramming/Fibonaci.java
+++ src/DynamicProgramming/Fibonaci.java
@@ -5,23 +5,23 @@ import java.util.Scanner;
 
 public class Fibonaci {
 
-    public static int fibb1(int n) {
+    public static long fibb1(int n) {
         if (n == 0 || n == 1) {
             return n;
         }
         return fibb1(n - 1) + fibb1(n - 2);
     }
-public static int fibb2(int n,int[] dp) {
+public static long fibb2(int n,long[] dp) {
         if (n == 0 || n == 1) {
             return n;
         }
-        int ans1,ans2;
-        if(dp[n-1]==-1){
+        long ans1,ans2;
+        if(dp[n-1]==0){
             ans1=fibb2(n-1,dp);
             dp[n-1]=ans1;
         }else {
             ans1=dp[n-1];
-        }if(dp[n-2]==-1){
+        }if(dp[n-2]==0){
             ans2=fibb2(n-2,dp);
             dp[n-2]=ans2;
         }else {
@@ -34,8 +34,8 @@ public static int fibb2(int n,int[] dp) {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         System.out.println(fibb1(n));
-        int[] dp=new int[n];
-        Arrays.fill(dp, -1);
+        long[] dp=new long[n];
+       // Arrays.fill(dp, -1);
         System.out.println(fibb2(n,dp));
     }
 }
