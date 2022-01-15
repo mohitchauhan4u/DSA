17458a0362637445719b2d04a8cd0f77da9597e3
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-15 16:19:22 +0530

    started MinimumNumberOfSquares.java

---
+++ src/DynamicProgramming/MinimumNumberOfSquares.java
@@ -0,0 +1,14 @@
+package DynamicProgramming;
+
+public class MinimumNumberOfSquares {
+
+    public static int minNumOfSquares(int n){
+
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
