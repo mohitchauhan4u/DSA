bb46657b8c7455409c283a6b02b516216a5f759f
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-15 16:48:21 +0530

    solved MinimumNumberOfSquares.java by recursive approach

--- src/DynamicProgramming/MinimumNumberOfSquares.java
+++ src/DynamicProgramming/MinimumNumberOfSquares.java
@@ -1,14 +1,32 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class MinimumNumberOfSquares {
-
-    public static int minNumOfSquares(int n){
-
-
-        return -1;
+//recursive
+    public static int minNumOfSquares(int n,int[] dp){
+        if(n==0){
+            return 0;
+        }
+        int minAns=Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){
+            int currentValue;
+            if(dp[n-(i*i)]==-1){
+                currentValue=minNumOfSquares(n-(i*i),dp);
+            }else {
+                currentValue=dp[n-(i*i)];
+            }
+            if(minAns>currentValue){
+                minAns=currentValue;
+            }
+        }
+        return 1+minAns;
     }
 
     public static void main(String[] args) {
-
+        int n=19;
+        int[]dp=new int[n+1];
+        Arrays.fill(dp,-1);
+        System.out.println(minNumOfSquares(n,dp));
     }
 }
