d99a47db95b15f17f48a089bd149523e83ae4ec6
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-15 14:24:50 +0530

    write wrong code

--- src/DynamicProgramming/MinStepsTo1.java
+++ src/DynamicProgramming/MinStepsTo1.java
@@ -22,45 +22,72 @@ public class MinStepsTo1 {
         return 1 + Math.min(subtractingByOne, Math.min(dividingByThree, dividingByTwo));
     }
 
-    public static int minStepsTo1WithDP(int n,int[] dp) {
+    //recursive
+    public static int minStepsTo1WithDP(int n, int[] dp) {
         if (n == 1) {
             return 0;
         }
         int subtractingByOne;
         int dividingByTwo = Integer.MAX_VALUE;
         int dividingByThree = Integer.MAX_VALUE;
-        if(dp[n-1]==-1){
-            subtractingByOne = minStepsTo1WithDP(n - 1,dp);
-            dp[n-1]=subtractingByOne;
-        }else {
-            subtractingByOne=dp[n-1];
+        if (dp[n - 1] == -1) {
+            subtractingByOne = minStepsTo1WithDP(n - 1, dp);
+            dp[n - 1] = subtractingByOne;
+        } else {
+            subtractingByOne = dp[n - 1];
         }
         if (n % 2 == 0) {
-            if(dp[n/2]==-1){
-                dividingByTwo = minStepsTo1WithDP(n / 2,dp);
-                dp[n/2]=dividingByTwo;
-            }else {
-                dividingByTwo=dp[n/2];
+            if (dp[n / 2] == -1) {
+                dividingByTwo = minStepsTo1WithDP(n / 2, dp);
+                dp[n / 2] = dividingByTwo;
+            } else {
+                dividingByTwo = dp[n / 2];
             }
         }
         if (n % 3 == 0) {
-            if(dp[n/3]==-1){
-                dividingByThree = minStepsTo1WithDP(n / 3,dp);
-                dp[n-1]=dividingByThree;
-            }else {
-                dividingByThree=dp[n/3];
-            }        }
+            if (dp[n / 3] == -1) {
+                dividingByThree = minStepsTo1WithDP(n / 3, dp);
+                dp[n - 1] = dividingByThree;
+            } else {
+                dividingByThree = dp[n / 3];
+            }
+        }
         return 1 + Math.min(subtractingByOne, Math.min(dividingByThree, dividingByTwo));
 
     }
+// iterative
+
+//    public static int minStepsTo1WithDP(int n) {
+//        if (n == 1) {
+//            return 0;
+//        }
+//        int[] dp = new int[n + 1];
+//        dp[0] = 0;
+//        dp[1] = 0;
+//        for (int i = 2; i <= n; i++) {
+//            int subtractingByOne;
+//            int dividingByTwo = Integer.MAX_VALUE;
+//            int dividingByThree = Integer.MAX_VALUE;
+//            subtractingByOne = dp[n - 1];
+//            if (i % 2 == 0) {
+//                dividingByTwo = dp[i / 2];
+//            }
+//            if (i % 3 == 0) {
+//                dividingByThree = dp[i / 3];
+//            }
+//            dp[i] = 1 + Math.min(subtractingByOne, Math.min(dividingByThree, dividingByTwo));
+//        }
+//        return dp[n];
+//    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter value of n");
         int n = sc.nextInt();
         System.out.println(minStepsTo1WithoutDP(n));
-        int[]dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        System.out.println(minStepsTo1WithDP(n,dp));
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        System.out.println(minStepsTo1WithDP(n, dp));
+        //System.out.println(minStepsTo1WithDP(n));
     }
 }
