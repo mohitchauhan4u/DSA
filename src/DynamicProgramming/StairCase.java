5a7bc66c7ac63fc2ff57c76659c2528cc0333a93
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-15 12:30:57 +0530

    added user input instead of custom input StairCase.java

--- src/DynamicProgramming/StairCase.java
+++ src/DynamicProgramming/StairCase.java
@@ -1,23 +1,29 @@
 package DynamicProgramming;
 
+import java.util.Scanner;
+
 public class StairCase {
-    public static int stairCase(int n){
-        if(n==0){
+    public static int stairCase(int n) {
+        if (n == 0) {
             return 1;
         }
-        if(n==1 || n==2){
+        if (n == 1 || n == 2) {
             return n;
         }
-        int[] dp=new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        dp[2]=2;
-        for(int i=3;i<n+1;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
         }
-         return dp[n];
+        return dp[n];
     }
+
     public static void main(String[] args) {
-        System.out.println(stairCase(10));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n");
+        int n = sc.nextInt();
+        System.out.println(stairCase(n));
     }
 }
