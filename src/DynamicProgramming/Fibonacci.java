28e8389d56ed2191da51221f50c77a9b880f7075
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-15 10:46:03 +0530

    formatted Fibonacci.java

--- src/DynamicProgramming/Fibonacci.java
+++ src/DynamicProgramming/Fibonacci.java
@@ -10,6 +10,7 @@ public class Fibonacci {
         }
         return fib1(n - 1) + fib1(n - 2);
     }
+
     // recursive dp O(n)
     public static long fib2(int n, long[] dp) {
         if (n == 0 || n == 1) {
@@ -36,18 +37,19 @@ public class Fibonacci {
         if (dp[n - 2] == 0) {
             dp[n - 2] = fib2(n - 2, dp);
         }
-        dp[n]=dp[n-1]+dp[n-2];
+        dp[n] = dp[n - 1] + dp[n - 2];
         return dp[n];
     }
+
     // iterative dp
-    public static long fib3(int n,long[] dp){
-        if(n==0 || n==1){
+    public static long fib3(int n, long[] dp) {
+        if (n == 0 || n == 1) {
             return n;
         }
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
@@ -56,7 +58,7 @@ public class Fibonacci {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         System.out.println(fib1(n));
-        long[] dp = new long[n+1];
+        long[] dp = new long[n + 1];
         // Arrays.fill(dp, -1);
         System.out.println(fib2(n, dp));
         System.out.println(fib3(n, dp));
