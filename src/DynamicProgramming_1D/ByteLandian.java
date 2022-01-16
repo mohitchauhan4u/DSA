26715746e0a7dd621d786ba0e2a0f55e4921fbc7
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-16 16:31:32 +0530

    started ByteLandian.java problem

---
+++ src/DynamicProgramming_1D/ByteLandian.java
@@ -0,0 +1,21 @@
+package DynamicProgramming_1D;
+
+import java.util.Scanner;
+
+public class ByteLandian {
+
+    public static int byteLandian(int n){
+
+
+
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        System.out.println(byteLandian(n));
+    }
+}
