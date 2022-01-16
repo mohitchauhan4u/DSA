59ff3d310ce33646e448630c39f9dd77e0157d5f
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-16 22:03:59 +0530

    completely solved ByteLandian.java problem by iterative approach

--- src/DynamicProgramming_1D/ByteLandian.java
+++ src/DynamicProgramming_1D/ByteLandian.java
@@ -1,19 +1,23 @@
 package DynamicProgramming_1D;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ByteLandian {
 
-    public static int byteLandian(int n){
-
-        return -1;
+    //iterative solution
+    public static int byteLandian(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i] = i;
+            dp[i] = Math.max(dp[i], dp[i / 2] + dp[i / 3] + dp[i / 4]);
+        }
+        return dp[n];
     }
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter value of n");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         System.out.println(byteLandian(n));
     }
 }
