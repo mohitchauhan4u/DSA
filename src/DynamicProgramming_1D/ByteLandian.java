65c3a63a3b1b46fb562aeec524bb7fdff539d729
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-16 16:47:52 +0530

    trying to solve  ByteLandian.java problem

--- src/DynamicProgramming_1D/ByteLandian.java
+++ src/DynamicProgramming_1D/ByteLandian.java
@@ -1,21 +1,19 @@
 package DynamicProgramming_1D;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ByteLandian {
 
     public static int byteLandian(int n){
 
-
-
-
         return -1;
     }
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter value of n");
         int n=sc.nextInt();
-
         System.out.println(byteLandian(n));
     }
 }
