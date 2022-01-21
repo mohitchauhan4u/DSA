74c05145258e0bba6e745b4178587aca3c575da6
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-21 22:30:07 +0530

    starting solving LeetcodeAddTwoNumber.java

--- src/DynamicProgramming_2D/MinCostPathProblem.java
+++ src/DynamicProgramming_2D/MinCostPathProblem.java
@@ -15,6 +15,6 @@ public class MinCostPathProblem {
     }
 
     public static void main(String[] args) {
-
+    
     }
 }
---
+++ src/OnlineQuestions/LeetcodeAddTwoNumber.java
@@ -0,0 +1,19 @@
+package OnlineQuestions;
+
+class ListNode{
+    int val;
+    int next;
+    public ListNode(int val){
+        this.val=val;
+    }
+}
+class LeetCodeAddTwoNumber {
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(addTwoNumbers(null,null));
+    }
+}
