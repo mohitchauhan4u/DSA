5c8c96d78af460e1a87ddb6a70b0e37fee2b3092
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-22 09:21:28 +0530

    solved  LeetcodeAddTwoNumber.java with both recursive and iterative approach

--- src/OnlineQuestions/LeetcodeAddTwoNumber.java
+++ src/OnlineQuestions/LeetcodeAddTwoNumber.java
@@ -2,38 +2,59 @@ package OnlineQuestions;
 
 class ListNode{
     int val;
-    int next;
+    ListNode next;
     public ListNode(int val){
         this.val=val;
     }
-}
-class LeetCodeAddTwoNumber {
-
-   /*
-            //my wrong or error code
-   public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return addTwoNumbers(l1, l2,false,null,null);
+    public ListNode(){
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2, boolean carry,ListNode head,ListNode last) {
+    public ListNode(int val,ListNode next) {
+        this.val=val;
+        this.next=next;
+    }
+}
+class LeetCodeAddTwoNumber {
+    //my  code
+   public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return addTwoNumbers(l1, l2,false,null,null);
+    }
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2, boolean carry,ListNode head,ListNode last) {
         if(!carry && l1==null && l2==null){
             return head;
         }else{
             if(head==null){
-                head=new ListNode(( l1!=null?l1.val:0 + l2!=null?l2.val:0 + carry?1:0)%10);
+                head=new ListNode((( l1!=null?l1.val:0) + (l2!=null?l2.val:0) + (carry?1:0))%10);
                 last=head;
-                carry=( l1!=null?l1.val:0 + l2!=null?l2.val:0 + carry?1:0 )>=10;
+                carry=( (l1!=null?l1.val:0) + (l2!=null?l2.val:0)+ ((carry?1:0)) )>=10;
                 return addTwoNumbers(l1!=null?l1.next:null,l2!=null?l2.next:null,carry,head,last);
             }else{
-                last.next=new ListNode(( l1!=null?l1.val:0 + l2!=null?l2.val:0 + carry?1:0)%10);
+                last.next=new ListNode((( l1!=null?l1.val:0) +( l2!=null?l2.val:0) +( carry?1:0))%10);
                 last=last.next;
-                carry=( l1!=null?l1.val:0 + l2!=null?l2.val:0 + carry?1:0 )>=10;
+                carry=( (l1!=null?l1.val:0) + (l2!=null?l2.val:0) + (carry?1:0 ))>=10;
                 return addTwoNumbers(l1!=null?l1.next:null,l2!=null?l2.next:null,carry,head,last);
             }
         }
-    }*/
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+    }
+
+    public ListNode addTwoNumbersIterative(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
     }
 
 
