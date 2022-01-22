0e9ec60421b32921784015ce20043db050e3fac7
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-22 09:26:43 +0530

    modified code of LeetcodeAddTwoNumber.java with both recursive and iterative approach

--- src/OnlineQuestions/LeetcodeAddTwoNumber.java
+++ src/OnlineQuestions/LeetcodeAddTwoNumber.java
@@ -6,38 +6,29 @@ class ListNode{
     public ListNode(int val){
         this.val=val;
     }
-    public ListNode(){
-    }
 
-    public ListNode(int val,ListNode next) {
-        this.val=val;
-        this.next=next;
-    }
 }
 class LeetCodeAddTwoNumber {
     //my  code
    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         return addTwoNumbers(l1, l2,false,null,null);
     }
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2, boolean carry,ListNode head,ListNode last) {
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2, boolean carry,ListNode head,ListNode last) {
         if(!carry && l1==null && l2==null){
             return head;
         }else{
             if(head==null){
                 head=new ListNode((( l1!=null?l1.val:0) + (l2!=null?l2.val:0) + (carry?1:0))%10);
                 last=head;
-                carry=( (l1!=null?l1.val:0) + (l2!=null?l2.val:0)+ ((carry?1:0)) )>=10;
-                return addTwoNumbers(l1!=null?l1.next:null,l2!=null?l2.next:null,carry,head,last);
             }else{
                 last.next=new ListNode((( l1!=null?l1.val:0) +( l2!=null?l2.val:0) +( carry?1:0))%10);
                 last=last.next;
-                carry=( (l1!=null?l1.val:0) + (l2!=null?l2.val:0) + (carry?1:0 ))>=10;
-                return addTwoNumbers(l1!=null?l1.next:null,l2!=null?l2.next:null,carry,head,last);
             }
+            carry=( (l1!=null?l1.val:0) + (l2!=null?l2.val:0)+ ((carry?1:0)) )>=10;
+            return addTwoNumbers(l1!=null?l1.next:null,l2!=null?l2.next:null,carry,head,last);
         }
     }
-
-    public ListNode addTwoNumbersIterative(ListNode l1, ListNode l2) {
+    public static ListNode addTwoNumbersIterative(ListNode l1, ListNode l2) {
         ListNode dummyHead = new ListNode(0);
         ListNode p = l1, q = l2, curr = dummyHead;
         int carry = 0;
@@ -60,5 +51,6 @@ class LeetCodeAddTwoNumber {
 
     public static void main(String[] args) {
         System.out.println(addTwoNumbers(null,null));
+        System.out.println(addTwoNumbersIterative(null,null));
     }
 }
