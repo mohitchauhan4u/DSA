67f464ee3a6d5b42d2c59b3b26db35d7caa1fab9
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-23 18:45:53 +0530

    started solving EditDistance problem

---
+++ src/DynamicProgramming_2D/EditDistance.java
@@ -0,0 +1,15 @@
+package DynamicProgramming_2D;
+
+public class EditDistance {
+
+    public static int editDistanceProblem(String s1,String s2){
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(editDistanceProblem("mohit","rohit"));
+        System.out.println(editDistanceProblem("ab","adc"));
+        System.out.println(editDistanceProblem("whgtdwhgtdg","aswcfg"));
+    }
+}
