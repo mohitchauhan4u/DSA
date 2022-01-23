b585821bdf6f4f717f0c4348ff775df6f7a3662b
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-23 11:36:53 +0530

    solved LCS.java problem with dp and iteratively

--- src/DynamicProgramming_2D/LCS.java
+++ src/DynamicProgramming_2D/LCS.java
@@ -2,7 +2,21 @@ package DynamicProgramming_2D;
 
 public class LCS {
 
-
+    public static int LCSIterative(String s1, String s2) {
+        int m = s1.length();
+        int n = s2.length();
+        int[][] dp = new int[m+1][n+1];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[0][0];
+    }
     public static int problemLCS(String s1, String s2) {
         if (s1.length() == 0 || s2.length() == 0) {
             return 0;
@@ -34,5 +48,8 @@ public class LCS {
         System.out.println(problemLCS("MohitChauhan4u", "RohitChaudhary4u"));
         System.out.println(problemLCS("MohitChauhan4u", "RohitSorout4u"));
         System.out.println(problemLCS("Mohit", "Rohit"));
+        System.out.println(problemLCS("MohitChauhan4u", "RohitChaudhary4u"));
+        System.out.println(LCSIterative("MohitChauhan4u", "RohitSorout4u"));
+        System.out.println(LCSIterative("Mohit", "Rohit"));
     }
 }
