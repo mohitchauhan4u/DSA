157756bf553d646b99ed6900e63d36b285189423
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-01-23 10:51:35 +0530

    solved LCS.java problem without dp and recursively

--- src/DynamicProgramming_2D/LCS.java
+++ src/DynamicProgramming_2D/LCS.java
@@ -3,7 +3,25 @@ package DynamicProgramming_2D;
 public class LCS {
 
 
-    public static void main(String[] args) {
+    public static int problemLCS(String s1, String s2){
+        if(s1.length()==0 || s2.length()==0){
+            return 0;
+        }
+        return LCSHelper(s1,s2,0,0);
+    }
+    private static int LCSHelper(String s1,String s2,int i,int j){
+        if(i>=s1.length()||j>=s2.length()){
+            return 0;
+        }
+        if(s1.charAt(i)==s2.charAt(j)){
+            return 1+LCSHelper(s1,s2,i+1,j+1);
+        }
+        return Math.max(LCSHelper(s1,s2,i,j+1),LCSHelper(s1,s2,i+1,j));
+    }
 
+    public static void main(String[] args) {
+        System.out.println(problemLCS("MohitChauhan4u","RohitChaudhary4u"));
+        System.out.println(problemLCS("MohitChauhan4u","RohitSorout4u"));
+        System.out.println(problemLCS("Mohit","Rohit"));
     }
 }
