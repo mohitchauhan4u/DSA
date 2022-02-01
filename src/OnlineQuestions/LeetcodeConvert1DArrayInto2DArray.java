bd67e0fc885227646a69e42ec8623d933c0000ea
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-02-01 12:00:45 +0530

    solving LeetcodeConvert1DArrayInto2DArray

---
+++ src/OnlineQuestions/LeetcodeConvert1DArrayInto2DArray.java
@@ -0,0 +1,24 @@
+package OnlineQuestions;
+
+import java.util.Arrays;
+
+public class LeetcodeConvert1DArrayInto2DArray {
+    public static int[][] construct2DArray(int[] arr, int m, int n) {
+        int[][] ans=new int[m][n];
+        int i=0;
+        int j=0;
+        for (int value : arr) {
+            if (j + 1 == n) {
+                i++;
+                j = 0;
+            }
+            ans[i][j] = value;
+            j++;
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(construct2DArray(new int[]{1, 2, 3, 4}, 2,2)));
+    }
+}
