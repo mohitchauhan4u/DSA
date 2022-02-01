b575bfdbaf1c9b5e8c233716eaa083bae941e90b
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-02-01 14:10:36 +0530

    solved CoinChange.java

--- src/DynamicProgramming_2D/CoinChange.java
+++ src/DynamicProgramming_2D/CoinChange.java
@@ -3,12 +3,24 @@ package DynamicProgramming_2D;
 public class CoinChange {
 
     public static int coinChangeProblem(int target,int[] arr){
-
-        return -1;
+        int[] dp=new int[target+1];
+        dp[0]=1;
+        for (int i=0;i< arr.length;i++){
+            for (int j=arr[i];j< dp.length;j++){
+                dp[j]+=dp[j-arr[i]];
+            }
+        }
+        return dp[target];
     }
     public static void main(String[] args) {
-        int[] arr={1,2,3};
-        int target=11;
-        System.out.println(coinChangeProblem(target,arr));
+        int[] arr1={1,2,3};
+        int target1=11;
+        System.out.println(coinChangeProblem(target1,arr1));
+        int[] arr2={2,3,5};
+        int target2=7;
+        System.out.println(coinChangeProblem(target2,arr2));
+        int[] arr3={2,3};
+        int target3=11;
+        System.out.println(coinChangeProblem(target3,arr3));
     }
 }
