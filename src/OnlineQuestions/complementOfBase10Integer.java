e64c58885efb2af0c9af9b01ee2d48b5f2a58c12
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-02 21:15:34 +0530

    ✔✔

--- src/OnlineQuestions/complementOfBase10Integer.java
+++ src/OnlineQuestions/complementOfBase10Integer.java
@@ -2,6 +2,12 @@ package OnlineQuestions;
 
 public class complementOfBase10Integer {
 
+    /**
+     * Convert the binary representation of a number to its complement
+     *
+     * @param n the integer to be complemented
+     * @return The bitwise complement of the given number.
+     */
     public static int bitwiseComplement(int n) {
         String str = Integer.toBinaryString(n);
         StringBuilder comp = new StringBuilder();
@@ -15,6 +21,10 @@ public class complementOfBase10Integer {
         return Integer.parseInt(comp.toString(), 2);
     }
 
+    /**
+     * Given a positive integer, return its complement number. The complement strategy is to flip the bits of its binary
+     * representation
+     */
     public static void main(String[] args) {
         System.out.println(bitwiseComplement(50));
         System.out.println(bitwiseComplement(5));
