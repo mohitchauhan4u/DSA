155da109952f59ec9fc418e5a912cc7f458a858d
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-07 23:12:50 +0530

    solved MagicNumber.java

---
+++ src/OnlineQuestions/MagicNumber.java
@@ -0,0 +1,34 @@
+package OnlineQuestions;
+
+public class MagicNumber {
+    /**
+     * If the sum of the digits of a number is 1, then the number is a magic number
+     *
+     * @param num The number to check.
+     */
+    public static void magicNumber(int num) {
+        int sumOfDigits = 0;
+        while (num > 0 || sumOfDigits > 9) {
+            if (num == 0) {
+                num = sumOfDigits;
+                sumOfDigits = 0;
+            }
+            sumOfDigits += num % 10;
+            num /= 10;
+        }
+
+        // If sum is 1, original number is magic number
+        if (sumOfDigits == 1) {
+            System.out.println("Magic number");
+        } else {
+            System.out.println("Not magic number");
+        }
+    }
+
+    public static void main(String[] args) {
+        magicNumber(163);
+        magicNumber(123);
+        magicNumber(123456);
+        magicNumber(1);
+    }
+}
