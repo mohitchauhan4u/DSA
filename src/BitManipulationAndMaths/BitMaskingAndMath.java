ed9e6891ea8b9768345bd7f699cd5416eaace8e9
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-11 22:20:49 +0530

    Started BitMaskingAndMath.java

---
+++ src/BitManipulationAndMaths/BitMaskingAndMath.java
@@ -0,0 +1,10 @@
+package BitManipulationAndMaths;
+
+public class BitMaskingAndMath {
+    // left shift a<<b=a*2^b    number a left shifted b times
+    //right shift a>>b=a/2^b    number a right shifted b times
+
+    public static void main(String[] args) {
+
+    }
+}
