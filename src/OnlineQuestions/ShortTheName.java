34831f257717a1399083e71e01134f6bcf15dedf
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-14 20:37:45 +0530

    solved problem ShortTheName

--- src/OnlineQuestions/ShortTheName.java
+++ src/OnlineQuestions/ShortTheName.java
@@ -4,17 +4,32 @@ public class ShortTheName {
 
     public static String shortTheName(String str){
         StringBuilder ans=new StringBuilder();
+        for(int i=0;i<str.length();i++){
+            ans.append(namePart(str.substring(i)));
+            while(i<str.length() && str.charAt(i)!=' '){
+                i++;
+            }
+        }
 
-
-        return null;
+        return ans.toString();
     }
 
-    private static String namePart(String s){
-
-        return s;
+    private static String namePart(String str){
+        int i=0;
+        while(i<str.length()){
+            if(str.charAt(i)==' '){
+                return  str.substring(0,1);
+            }
+            i++;
+        }
+        return str;
     }
 
     public static void main(String[] args) {
         System.out.println(shortTheName("Mohit Chauhan"));
+        System.out.println(shortTheName("Koushal Kishor Garg"));
+        System.out.println(shortTheName("Prem Narayan Pandey"));
+        System.out.println(shortTheName("Krishan Chandra Pandey"));
+        System.out.println(shortTheName("Kunal Sharma Baman"));
     }
 }
