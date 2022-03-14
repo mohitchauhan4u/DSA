9276cf5dc4324a50e57e9e2df1028e460b097c03
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-14 20:25:21 +0530

    started problem ShortTheName

---
+++ src/OnlineQuestions/ShortTheName.java
@@ -0,0 +1,20 @@
+package OnlineQuestions;
+
+public class ShortTheName {
+
+    public static String shortTheName(String str){
+        StringBuilder ans=new StringBuilder();
+
+
+        return null;
+    }
+
+    private static String namePart(String s){
+
+        return s;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(shortTheName("Mohit Chauhan"));
+    }
+}
