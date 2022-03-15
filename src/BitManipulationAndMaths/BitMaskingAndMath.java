6f2acbb008838c507d6260d0b2892a5c891ef229
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-15 21:41:24 +0530

    solved oddOrNot

--- src/BitManipulationAndMaths/BitMaskingAndMath.java
+++ src/BitManipulationAndMaths/BitMaskingAndMath.java
@@ -4,7 +4,12 @@ public class BitMaskingAndMath {
     // left shift a<<b=a*2^b    number a left shifted b times
     //right shift a>>b=a/2^b    number a right shifted b times
 
-    public static void main(String[] args) {
+    public static boolean oddOrNot(int n) {
+        return (n&1)==1;
+    }
 
+    public static void main(String[] args) {
+        System.out.println(oddOrNot(13));
+        System.out.println(oddOrNot(136));
     }
 }
