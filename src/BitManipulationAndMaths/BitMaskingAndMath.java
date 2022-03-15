d91ac237c63d62a7c3616d40706910818596646d
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-15 21:43:28 +0530

    solved evenOrNot

--- src/BitManipulationAndMaths/BitMaskingAndMath.java
+++ src/BitManipulationAndMaths/BitMaskingAndMath.java
@@ -5,11 +5,15 @@ public class BitMaskingAndMath {
     //right shift a>>b=a/2^b    number a right shifted b times
 
     public static boolean oddOrNot(int n) {
-        return (n&1)==1;
+        return (n & 1) == 1;
+    }
+
+    public static boolean evenOrNot(int n) {
+        return (n & 1) == 0;
     }
 
     public static void main(String[] args) {
-        System.out.println(oddOrNot(13));
-        System.out.println(oddOrNot(136));
+        System.out.println(oddOrNot(13) + " " + evenOrNot(13));
+        System.out.println(oddOrNot(136) + " " + evenOrNot(136));
     }
 }
