516716233a32c86b20b27a475576ac57e9eaa1fa
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-15 22:02:53 +0530

    solved findUniqueNumberInArray

--- src/BitManipulationAndMaths/BitMaskingAndMath.java
+++ src/BitManipulationAndMaths/BitMaskingAndMath.java
@@ -15,5 +15,15 @@ public class BitMaskingAndMath {
     public static void main(String[] args) {
         System.out.println(oddOrNot(13) + " " + evenOrNot(13));
         System.out.println(oddOrNot(136) + " " + evenOrNot(136));
+        System.out.println(findUniqueNumberInArray(new int[]{1,2,3,1,3,5,5,6,6,7,9,7,9,8,8}));
+        System.out.println(findUniqueNumberInArray(new int[]{5,5,6,6,7,9,7,9,3,8,8}));
+    }
+
+    private static int findUniqueNumberInArray(int[] arr) {
+        int unique=0;
+        for(int n:arr){
+            unique^=n;
+        }
+        return unique;
     }
 }
