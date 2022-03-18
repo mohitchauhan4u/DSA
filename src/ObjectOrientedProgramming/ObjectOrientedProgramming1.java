f072e9050c540594353270d816c6270902d69ec6
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-18 18:41:17 +0530

    created student class

--- src/ObjectOrientedProgramming/ObjectOrientedProgramming1.java
+++ src/ObjectOrientedProgramming/ObjectOrientedProgramming1.java
@@ -2,8 +2,20 @@ package ObjectOrientedProgramming;
 
 public class ObjectOrientedProgramming1 {
 
-
     public static void main(String[] args) {
+        Student s1 = new Student(1, "Mohit", 99);
+        System.out.println(s1.rollNo + " " + s1.name + " " + s1.marks);
+    }
+}
 
+class Student {
+    int rollNo;
+    String name;
+    float marks;
+
+    Student(int rollNo, String name, float marks) {
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
     }
 }
