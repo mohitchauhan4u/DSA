3b899fa18ccbeb819b1eb4b3f3b944525563be0d
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-22 14:55:23 +0530

    solved gfg problem

---
+++ src/OnlineQuestions/GFGArray.java
@@ -0,0 +1,20 @@
+package OnlineQuestions;
+
+import java.util.Arrays;
+
+public class GFGArray {
+
+    public static int[] minAndMax(int[] arr) {
+        int min = arr[0], max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (min > arr[i]) min = arr[i];
+            if (max < arr[i]) max = arr[i];
+        }
+
+        return new int[]{min, max};
+    }
+
+    public static void main(String[] args) {
+        System.out.println((Arrays.toString(minAndMax(new int[]{1, 2, 5, 2, 9839, 8, 31, 3, 24, 35, 135, 24, 23, 234, 235, 1, 34, 124, 225, 235, 24, 62, 4, -123, 1, 3, -1, 2, 356, -24, 5 , 12431, 142, 24}))));
+    }
+}
