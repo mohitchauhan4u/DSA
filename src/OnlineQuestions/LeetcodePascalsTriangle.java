4599d165f7a502571d3eda086e5dac8e9ab43f63
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-24 08:51:42 +0530

    solved LeetcodePascalsTriangle

--- src/OnlineQuestions/LeetcodePascalsTriangle.java
+++ src/OnlineQuestions/LeetcodePascalsTriangle.java
@@ -6,30 +6,32 @@ import java.util.List;
 public class LeetcodePascalsTriangle {
 
     public static List<List<Integer>> generate(int numRows) {
-        int size=1;
-        List<List<Integer>> list=new ArrayList<>();
-        List<Integer> newList=new ArrayList<Integer>(size);
+        int size = 1;
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> newList = new ArrayList<>(size);
         newList.add(1);
-        list.add(size-1,newList);
+        list.add(0, newList);
         size++;
-        if(numRows==1) return list;
-        newList=new ArrayList<Integer>(size);
+        if (numRows == 1) return list;
+        newList = new ArrayList<>(size);
         newList.add(1);
         newList.add(1);
-        list.add(size-1,newList);
-        if(numRows==2) return list;
+        list.add(1, newList);
+        if (numRows == 2) return list;
         size++;
-        List<Integer> prev=newList;
-        while(size<=numRows){
-            newList=new ArrayList<Integer>(size);
-            newList.add(0,1);
-            newList.add(size-1,1);
-            for(int i=1;i<size-1;i++){
-                newList.add(i,prev.get(i-1)+prev.get(i));
+        List<Integer> prev = newList;
+        while (size <= numRows) {
+            newList = new ArrayList<>(size);
+            newList.add(0, 1);
+            newList.add(newList.size()-1, 1);
+            int i = 1;
+            while (i < size - 1) {
+                newList.add(i, prev.get(i - 1) + prev.get(i));
+                i++;
             }
-            list.add(size-1,newList);
+            list.add(size - 1, newList);
             size++;
-            prev=newList;
+            prev = newList;
         }
         return list;
     }
