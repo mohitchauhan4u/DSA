4300a0e14b288f2a395b33b8715bd9eef8e1c166
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-03-29 17:23:28 +0530

    solved GFGArray.java rearrangeArrayInAlternatingPositiveAndNegativeItems

--- src/OnlineQuestions/GFGArray.java
+++ src/OnlineQuestions/GFGArray.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 
 public class GFGArray {
 
+    public static int[] rearrangeArrayInAlternatingPositiveAndNegativeItems(int[] arr){
+
+        return arr;
+    }
     public static int[] moveAllNegativeNumbersToBeginning(int[] arr) {
         int positiveIndex = 0;
         while (positiveIndex < arr.length && arr[positiveIndex] < 0) {
@@ -32,6 +36,7 @@ public class GFGArray {
 
     public static void main(String[] args) {
         //System.out.println((Arrays.toString(minAndMax(new int[]{1, 2, 5, 2, 9839, 8, 31, 3, 24, 35, 135, 24, 23, 234, 235, 1, 34, 124, 225, 235, 24, 62, 4, -123, 1, 3, -1, 2, 356, -24, 5, 12431, 142, 24}))));
-        System.out.println(Arrays.toString(moveAllNegativeNumbersToBeginning(new int[]{1, -5, 2, -3, 4, 365, -1, -129, 5, 6, -7, 8})));
+        //System.out.println(Arrays.toString(moveAllNegativeNumbersToBeginning(new int[]{1, -5, 2, -3, 4, 365, -1, -129, 5, 6, -7, 8})));
+        System.out.println(Arrays.toString(rearrangeArrayInAlternatingPositiveAndNegativeItems(new int[]{1, -5, 2, -3, 4, 365, -1, -129, 5, 6, -7, 8,-2,11,56,247,12,-26})));
     }
 }
