a7447f20f56454339cdbe36f373e0d9fcd0479e0
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-04-08 21:59:43 +0530

    solved GFGString.java getPhoneNumber

--- src/OnlineQuestions/GFGString.java
+++ src/OnlineQuestions/GFGString.java
@@ -1,5 +1,7 @@
 package OnlineQuestions;
 
+import java.util.HashMap;
+
 public class GFGString {
 
     public static void printAllTheDuplicatesInTheInputString(String s) {
@@ -14,8 +16,49 @@ public class GFGString {
         System.out.println();
     }
 
+    public static String getPhoneNumber(String s) {
+        StringBuilder str = new StringBuilder();
+        HashMap<String, String> map = new HashMap<>();
+        map.put("one", "1");
+        map.put("two", "2");
+        map.put("three", "3");
+        map.put("four", "4");
+        map.put("five", "5");
+        map.put("six", "6");
+        map.put("seven", "7");
+        map.put("eight", "8");
+        map.put("nine", "9");
+        map.put("zero", "0");
+        int count = 1;
+        for (int i = 0; i < s.length(); i++) {
+            StringBuilder temp = new StringBuilder();
+            while (i < s.length() && s.charAt(i) != ' ') {
+                temp.append(s.charAt(i));
+                i++;
+            }
+
+            if (temp.toString().equals("double")) {
+                count = 2;
+            } else if (temp.toString().equals("triple")) {
+                count = 3;
+            } else {
+                helper(str, map.get(temp.toString()), count);
+                count = 1;
+            }
+        }
+        return str.toString();
+    }
+
+    public static void helper(StringBuilder str, String temp, int n) {
+        while (n > 0) {
+            str.append(temp);
+            n--;
+        }
+    }
+
     public static void main(String[] args) {
-        printAllTheDuplicatesInTheInputString("Mohit Chauhan");
-        printAllTheDuplicatesInTheInputString("Mohit Rohit Sohit");
+//        printAllTheDuplicatesInTheInputString("Mohit Chauhan");
+//        printAllTheDuplicatesInTheInputString("Mohit Rohit Sohit");
+        System.out.println(getPhoneNumber("one one double zero"));
     }
 }
