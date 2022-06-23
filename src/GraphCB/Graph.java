dc44bec4e886af7f12b1530e06906bff32e3c333
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-23 16:59:43 +0530

    took input from user

--- src/GraphCB/Graph.java
+++ src/GraphCB/Graph.java
@@ -1,7 +1,20 @@
 package GraphCB;
 
+import java.util.Scanner;
+
 public class Graph {
     public static void main(String[] args) {
-
+    Scanner sc=new Scanner(System.in);
+        System.out.println("Enter no of vertex");
+        int v=sc.nextInt();
+        System.out.println("Enter no of edges");
+        int e=sc.nextInt();
+        int[][] adj_mat=new int[v][v];
+        for(int i=0;i<e;i++){
+            int sv=sc.nextInt();
+            int ev=sc.nextInt();
+            adj_mat[sv][ev]=1;
+            adj_mat[ev][sv]=1;
+        }
     }
 }
