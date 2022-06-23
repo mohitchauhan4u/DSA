80b444d98d267e41e2b8f9cac26ebdd2bcd8a6d9
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-23 17:09:37 +0530

    added function to print graph

--- src/GraphCB/Graph.java
+++ src/GraphCB/Graph.java
@@ -3,6 +3,25 @@ package GraphCB;
 import java.util.Scanner;
 
 public class Graph {
+
+    public static void printHelper(int adj_mat[][],int sv,boolean[] visited){
+        System.out.println(sv);
+        int v= adj_mat.length;
+        for(int i=0;i<v;i++){
+            if(adj_mat[sv][i]==1 && !visited[i]){
+                printHelper(adj_mat, i,visited);
+            }
+        }
+    }
+    public  static void print(int adj_mat[][],int sv){
+        boolean[] visited=new boolean[adj_mat.length];
+        for(int i=0;i< adj_mat.length;i++) {
+            if(!visited[i]){
+                printHelper(adj_mat, i, visited);
+            }
+        }
+    }
+
     public static void main(String[] args) {
     Scanner sc=new Scanner(System.in);
         System.out.println("Enter no of vertex");
@@ -16,5 +35,6 @@ public class Graph {
             adj_mat[sv][ev]=1;
             adj_mat[ev][sv]=1;
         }
+        //print(adj_mat,0);
     }
 }
